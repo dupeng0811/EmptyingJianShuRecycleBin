b26ae3335bbc50020e03256cf4985cbd4208ed20
Dupeng <dupeng0811@qq.com>
2022-02-09 17:15:51 +0800

    简单处理简书页面报错不能继续执行的问题

--- EmptyRecycleBin.py
+++ EmptyRecycleBin.py
@@ -4,14 +4,19 @@ import time
 option = webdriver.ChromeOptions()
 option.add_argument(r'user-data-dir=C:\Users\Dupeng\AppData\Local\Google\Chrome\User Data\Default')
 # driver = webdriver.Chrome(options = option)
-driver = webdriver.Chrome('C:\Work\我的项目\EmptyingJianShuRecycleBin\chromedriver.exe',options=option)
+driver = webdriver.Chrome('C:\Work\我的项目\EmptyingJianShuRecycleBin\chromedriver.exe', options=option)
 driver.implicitly_wait(10)
 driver.get("https://www.jianshu.com/writer#/recycle/")
 
 sreach_windows = driver.current_window_handle
 time.sleep(10)
+times = 1
 while 1:
     driver.get("https://www.jianshu.com/writer#/recycle/")
     driver.find_element_by_xpath('//*[@id="root"]/div/div[2]/div[3]/span[2]').click()
-    time.sleep(5)
-
+    if times % 10 == 0:
+        times = 1
+        driver.refresh()
+        time.sleep(10)
+    time.sleep(2)
+    times = times + 1
